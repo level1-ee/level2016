16a9fd2ce7897550bd0dcb9cbc70502b6e06276f
Raik Ilves <r@raik.me>
2018-01-01 18:13:26 +0200

    init

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+  // Also can pass in optional settings block
+  var rellax = new Rellax('.rellax');
+  new Vivus('illustration-team', {duration: 200});
+  new Vivus('illustration-videos', {duration: 200});
+  new Vivus('illustration-posts', {duration: 200});
+  new Vivus('illustration-podcast', {duration: 200});
+  new Vivus('illustration-moo', {duration: 200});
